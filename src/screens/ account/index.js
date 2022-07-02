67166521013751e258645b5b82c06a23930e7682
Duc.do <duc.do@timeedit.com>
2022-07-02 15:33:12 +0700

    update code

--- src/screens/ account/index.js
+++ src/screens/ account/index.js
@@ -1,5 +1,5 @@
-import { Pressable, StyleSheet, Dimensions } from 'react-native';
-import React, { useState } from 'react';
+import { Pressable, StyleSheet, Dimensions, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image } from 'react-native-ui-lib';
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -13,16 +13,45 @@ const { height, width } = Dimensions.get('window');
 
 const Account = ({ route }) => {
   const navigation = useNavigation();
-  const { otherParam } = route.params;
-
   const _goBack = () => {
     navigation.goBack();
   }
 
+  const [userData, setUserData] = useState({});
+  const [err, setErr] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    _getUserData();
+  }, []);
+
+  _getUserData = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get('http://localhost:3000/nodejs/nodejs09-10')
+      setUserData(data)
+    } catch (err) {
+      setErr(err);
+      console.log('err', err)
+      Alert.alert("call api error")
+    } finally {
+
+      setLoading(false);
+    }
+  }
+
+
   return (
     <View flex center>
-      <Pressable onPress={_goBack}>
-        <Text>go back</Text>
+      <Pressable>
+        <Text>Account page</Text>
+        {loading ? <Text>loading ...</Text> : null}
+        {_.isEmpty(userData) ? null : <View>
+          {Object.keys(userData).map(key => {
+            return <Text>{key}: {userData[key]}</Text>
+          })}
+        </View>}
+
       </Pressable>
     </View>
   );
